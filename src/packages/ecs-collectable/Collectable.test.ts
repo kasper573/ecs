3a704bd35c2b7a1f5d073f84fd2b3fd1a358f2d3
Kasper Sandin <kasper.sandin@gmail.com>
2021-01-03 22:08:20 +0100

    Create unit tests for Collectable

--- src/packages/ecs-collectable/Collectable.test.ts
+++ src/packages/ecs-collectable/Collectable.test.ts
@@ -1,6 +1,29 @@
 import { Collectable } from "./Collectable";
+import { World } from "../ecs/World";
+import { Entity } from "../ecs/Entity";
+import { Inventory } from "./Inventory";
 
-new Collectable();
+describe("Collectable", () => {
+  test("Picking up a Collectable entity removes it from the scene", () => {
+    const { entity, world } = pickUp();
+    expect(world.scene.includes(entity)).toBe(false);
+  });
 
-test("picking up a Collectable entity removes it from the scene", () => {});
-test("a Collectable entity in your inventory has its pick up action disabled", () => {});
+  test("A Collectable entity in your inventory has its pick up action disabled", () => {
+    const { world, action } = pickUp();
+    expect(world.actions).not.toContainEqual(action);
+  });
+});
+
+const pickUp = () => {
+  const entity = new Entity("entity", {}, () => [new Collectable()]);
+  const world = new World({
+    sceneId: "a",
+    state: { inventory: new Inventory() },
+    scenes: { a: [entity] },
+  });
+  const actionName = Collectable.prototype.action.call(null, entity);
+  const action = world.actions.find((action) => action.name === actionName)!;
+  action.perform(world);
+  return { entity, world, action };
+};
