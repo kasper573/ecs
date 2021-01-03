73d2569dcabe1b3a8efe05df87d94bde9413727c
Kasper Sandin <kasper.sandin@gmail.com>
2021-01-03 22:22:09 +0100

    Create unit tests for Describable interaction with Collectable

--- src/packages/ecs-collectable/Describable.test.ts
+++
@@ -1,6 +0,0 @@
-import { Describable } from "./Describable";
-
-new Describable({ describe: () => "" });
-
-test("inventory entities are not described", () => {});
-test("inventory entities actions are enabled", () => {});
---
+++ src/packages/ecs-collectable/ecs-collectable.test.ts
@@ -0,0 +1,48 @@
+import { Collectable } from "./Collectable";
+import { World } from "../ecs/World";
+import { Entity } from "../ecs/Entity";
+import { Inventory } from "./Inventory";
+import { Describable } from "./Describable";
+import { describeEntity } from "../ecs-text/describeEntities";
+
+describe("Collectable", () => {
+  test("Picking up a Collectable entity removes it from the scene", () => {
+    const { entity, world, pickUp } = setup();
+    pickUp.perform(world);
+    expect(world.scene.includes(entity)).toBe(false);
+  });
+
+  test("A Collectable entity in your inventory has its pick up action disabled", () => {
+    const { world, pickUp } = setup();
+    pickUp.perform(world);
+    expect(world.actions).not.toContainEqual(pickUp);
+  });
+});
+
+describe("Describable", () => {
+  test("Collectable entities in the scene are described", () => {
+    const { entity, world } = setup();
+    expect(describeEntity(entity, world)).toContain("A visible entity");
+  });
+
+  test("Collectable entities in the inventory are not described", () => {
+    const { entity, world, pickUp } = setup();
+    pickUp.perform(world);
+    expect(describeEntity(entity, world)).not.toContain("A visible entity");
+  });
+});
+
+const setup = () => {
+  const entity = new Entity("entity", {}, () => [
+    new Describable({ describe: () => "A visible entity" }),
+    new Collectable(),
+  ]);
+  const world = new World({
+    sceneId: "a",
+    state: { inventory: new Inventory() },
+    scenes: { a: [entity] },
+  });
+  const pickUpName = Collectable.prototype.action.call(null, entity);
+  const pickUp = world.actions.find((action) => action.name === pickUpName)!;
+  return { entity, world, pickUp };
+};
