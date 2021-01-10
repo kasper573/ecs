d5051f035af8b8e956a42a76be1a0791f7f99005
Kasper Sandin <kasper.sandin@gmail.com>
2021-01-10 19:34:05 +0100

    Make command interpretation case insensitive

--- src/packages/ecs-interactive/interpretCommand.ts
+++ src/packages/ecs-interactive/interpretCommand.ts
@@ -1,4 +1,4 @@
 import { Action } from "./Action";
 
 export const interpretCommand = (command: string, actions: Action[]) =>
-  actions.find((action) => action.name === command);
+  actions.find((action) => action.name.toLowerCase() === command.toLowerCase());
