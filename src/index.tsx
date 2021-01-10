02368551fa9b045ed9ddb9e3bb1093704da03b17
Kasper Sandin <kasper.sandin@gmail.com>
2021-01-10 19:33:19 +0100

    Convert twitch bot to an ECS module

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,29 @@ import App from "./packages/twitch-text-adventure/react/App";
 import reportWebVitals from "./reportWebVitals";
 import { theme } from "./packages/twitch-text-adventure/react/theme";
 import { createGame } from "./packages/twitch-text-adventure/createGame";
+import { TwitchIntegration } from "./packages/ecs-twitch-integration/TwitchIntegration";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App theme={theme} system={createGame()} />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const system = createGame();
+
+const twitchIntegration = new TwitchIntegration({
+  username: process.env.REACT_APP_TTA_BOT_USERNAME!,
+  token: process.env.REACT_APP_TTA_BOT_TOKEN!,
+  channel: process.env.REACT_APP_TTA_BOT_CHANNEL!,
+  onCommand: render,
+});
+system.modules.push(twitchIntegration);
+twitchIntegration.start();
+
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App theme={theme} system={system} />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
+
+render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/packages/ecs-twitch-integration/TwitchIntegration.ts
@@ -0,0 +1,53 @@
+import { Client as TmiClient } from "tmi.js";
+import { SystemModule } from "../ecs/SystemModule";
+import { performCommand } from "../ecs-interactive/performCommand";
+import { InteractionResult } from "../ecs-interactive/InteractionResult";
+
+export class TwitchIntegration extends SystemModule {
+  private tmiClient: TmiClient;
+  constructor({
+    channel,
+    username,
+    token,
+    onCommand,
+  }: TwitchIntegrationOptions) {
+    super();
+    this.tmiClient = new TmiClient({
+      options: { debug: true },
+      connection: {
+        secure: true,
+        reconnect: true,
+      },
+      identity: {
+        username,
+        password: token,
+      },
+      channels: [channel],
+    });
+
+    this.tmiClient.on("message", (channel, tags, message, self) => {
+      // Don't do anything without a system and ignore echoed messages.
+      if (!this.system || self) return;
+
+      const match = /!command (.+)/.exec(message);
+      if (this.system && match) {
+        onCommand(performCommand(this.system, match[1]));
+      }
+    });
+  }
+
+  start() {
+    return this.tmiClient.connect();
+  }
+
+  stop() {
+    return this.tmiClient.disconnect();
+  }
+}
+
+export type TwitchIntegrationOptions = {
+  username: string;
+  token: string;
+  channel: string;
+  onCommand: (result: InteractionResult | undefined) => void;
+};
