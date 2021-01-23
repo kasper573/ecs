0e6d9765231e8d4532dab42cb4af43bce98025ba
Kasper Sandin <kasper.sandin@gmail.com>
2021-01-23 01:03:57 +0100

    Add unit tests for vote events

--- src/packages/twitch-poll-chatbot/TwitchPollChatbot.test.ts
+++ src/packages/twitch-poll-chatbot/TwitchPollChatbot.test.ts
@@ -13,6 +13,7 @@ test("can define how to parse votes", () => {
     },
     async (bot, client) => {
       await bot.poll("que?", answers);
+
       client.events.emit("message", "", { username: "jestA" }, "bogus", false);
       expect(bot.winningVotes.length).toBe(0);
       client.events.emit("message", "", { username: "jestA" }, "first", false);
@@ -24,8 +25,10 @@ test("can define how to parse votes", () => {
 test("determineWinner() resets poll after returning the winner", () =>
   useBot(createBasicOptions(), async (bot, client) => {
     await bot.poll("que?", ["a", "b"]);
+
     client.events.emit("message", "", { username: "jestA" }, "1", false);
     client.events.emit("message", "", { username: "jestB" }, "2", false);
+
     await bot.determineWinner();
     expect(bot.votesPerAnswerIndex).toEqual([0, 0]);
     expect(bot.winningVotes).toEqual([]);
@@ -34,11 +37,17 @@ test("determineWinner() resets poll after returning the winner", () =>
 describe("determines the right winner", () => {
   test("when there is no tie breaker", () =>
     useBot(createBasicOptions(), async (bot, client) => {
+      const eventAnswerIndexes: number[] = [];
+      bot.events.on("vote", (index) => eventAnswerIndexes.push(index));
+
       await bot.poll("que?", ["first", "second"]);
+
       client.events.emit("message", "", { username: "jestA" }, "2", false);
       client.events.emit("message", "", { username: "jestB" }, "2", false);
       client.events.emit("message", "", { username: "jestC" }, "2", false);
       client.events.emit("message", "", { username: "jestD" }, "1", false);
+
+      expect(eventAnswerIndexes).toEqual([1, 1, 1, 0]);
       expect(bot.votesPerAnswerIndex).toEqual([1, 3]);
       expect(bot.winningVotes).toEqual([{ index: 1, count: 3 }]);
       expect(await bot.determineWinner()).toBe(1);
@@ -46,25 +55,36 @@ describe("determines the right winner", () => {
 
   test("when votes are cast outside the accepted range", () =>
     useBot(createBasicOptions(), async (bot, client) => {
+      const eventAnswerIndexes: number[] = [];
+      bot.events.on("vote", (index) => eventAnswerIndexes.push(index));
+
       await bot.poll("que?", ["first", "second"]);
+
       client.events.emit("message", "", { username: "jestA" }, "-1", false); // Outside
       client.events.emit("message", "", { username: "jestB" }, "-1", false); // Outside
       client.events.emit("message", "", { username: "jestC" }, "2", false);
       client.events.emit("message", "", { username: "jestD" }, "3", false); // Outside
       client.events.emit("message", "", { username: "jestE" }, "3", false); // Outside
+
+      expect(eventAnswerIndexes).toEqual([1]);
       expect(bot.votesPerAnswerIndex).toEqual([0, 1]);
       expect(bot.winningVotes).toEqual([{ index: 1, count: 1 }]);
       expect(await bot.determineWinner()).toBe(1);
     }));
 
   test("when there is a false tie breaker", () =>
-    // rephrase
     useBot(createBasicOptions(), async (bot, client) => {
+      const eventAnswerIndexes: number[] = [];
+      bot.events.on("vote", (index) => eventAnswerIndexes.push(index));
+
       await bot.poll("que?", ["first", "second", "third"]);
+
       client.events.emit("message", "", { username: "jestA" }, "1", false);
       client.events.emit("message", "", { username: "jestB" }, "2", false);
       client.events.emit("message", "", { username: "jestC" }, "2", false);
       client.events.emit("message", "", { username: "jestD" }, "3", false);
+
+      expect(eventAnswerIndexes).toEqual([0, 1, 1, 2]);
       expect(bot.votesPerAnswerIndex).toEqual([1, 2, 1]);
       expect(bot.winningVotes).toEqual([{ index: 1, count: 2 }]);
       expect(await bot.determineWinner()).toBe(1);
@@ -72,11 +92,17 @@ describe("determines the right winner", () => {
 
   test("when there is a tie breaker", () =>
     useBot(createBasicOptions(), async (bot, client) => {
+      const eventAnswerIndexes: number[] = [];
+      bot.events.on("vote", (index) => eventAnswerIndexes.push(index));
+
       await bot.poll("que?", ["first", "second"]);
+
       client.events.emit("message", "", { username: "jestA" }, "2", false);
       client.events.emit("message", "", { username: "jestB" }, "2", false);
       client.events.emit("message", "", { username: "jestC" }, "1", false);
       client.events.emit("message", "", { username: "jestD" }, "1", false);
+
+      expect(eventAnswerIndexes).toEqual([1, 1, 0, 0]);
       expect(bot.votesPerAnswerIndex).toEqual([2, 2]);
       expect(bot.winningVotes).toEqual([
         { index: 0, count: 2 },
@@ -87,12 +113,20 @@ describe("determines the right winner", () => {
 
   test("when there are no votes (should count as tie breaker)", () =>
     useBot(createBasicOptions(), async (bot) => {
+      const eventAnswerIndexes: number[] = [];
+      bot.events.on("vote", (index) => eventAnswerIndexes.push(index));
+
       await bot.poll("que?", ["first", "second"]);
+
+      expect(eventAnswerIndexes).toEqual([]);
       expect(bot.votesPerAnswerIndex).toEqual([0, 0]);
       expect(bot.winningVotes).toEqual([]);
       expect(await bot.determineWinner()).toBe(-1);
     }));
 });
+
+// options
+// silent/loud (write tests that make sure the chatbot send the expected messages)
 
 const useBot = async (
   options: TwitchPollChatboxOptions,
@@ -104,6 +138,7 @@ const useBot = async (
   await use(bot, client);
   await client.disconnect();
   bot.detach();
+  bot.events.removeAllListeners();
 };
 
 const createBasicOptions = () => ({
