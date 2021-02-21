fd35dc9709eae0326c9df1ed4725af8c08cf8b09
Kasper Sandin <kasper.sandin@gmail.com>
2021-02-21 21:47:32 +0100

    Add CssBaseline

--- src/packages/shared-components/App.tsx
+++ src/packages/shared-components/App.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren } from "react";
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
-import { MuiThemeProvider } from "@material-ui/core";
+import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
 import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
 
@@ -9,6 +9,7 @@ export type AppProps = PropsWithChildren<{}>;
 export const App = ({ children }: AppProps) => (
   <MuiThemeProvider theme={theme}>
     <StyledThemeProvider theme={theme}>
+      <CssBaseline />
       <GlobalStyle />
       {children}
     </StyledThemeProvider>
