d9163b0c029448d25e9e5629c46d76d476cc45a2
Kasper Sandin <kasper.sandin@gmail.com>
2021-02-24 20:24:45 +0100

    Use default background color in EditorScenePanel

--- src/packages/ecs-editor/EditorScenePanel.tsx
+++ src/packages/ecs-editor/EditorScenePanel.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
  * Displays the selected scene
  */
 export const EditorScenePanel = styled.div`
-  background: black;
+  background: ${({ theme }) => theme.palette.background.default};
   flex: 1;
   padding: ${({ theme }) => theme.spacing(3)}px;
 `;
