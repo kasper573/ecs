76f7ca3084f0669aeb74d8a714264090458a5633
Kasper Sandin <kasper.sandin@gmail.com>
2021-03-17 20:10:25 +0100

    Move common popper code to CommonPopper.tsx

---
+++ src/packages/ecs-editor/components/CommonPopper.tsx
@@ -0,0 +1,31 @@
+import React, { PropsWithChildren, ReactElement } from "react";
+import PopupState, { bindPopper, bindToggle } from "material-ui-popup-state";
+import { ClickAwayListener, Fade, Paper, Popper } from "@material-ui/core";
+
+export type CommonPopperProps = PropsWithChildren<{
+  toggle: (props: ReturnType<typeof bindToggle>) => ReactElement;
+}>;
+
+export const CommonPopper = ({
+  toggle: Toggle,
+  children,
+}: CommonPopperProps) => (
+  <PopupState variant="popper" popupId="demo-popup-popper">
+    {(popupState) => (
+      <div>
+        <Toggle {...bindToggle(popupState)} />
+        <Popper {...bindPopper(popupState)} placement="bottom-end" transition>
+          {({ TransitionProps }) => (
+            <Fade {...TransitionProps} timeout={350}>
+              <Paper style={{ width: 300 }}>
+                <ClickAwayListener onClickAway={popupState.close}>
+                  {children}
+                </ClickAwayListener>
+              </Paper>
+            </Fade>
+          )}
+        </Popper>
+      </div>
+    )}
+  </PopupState>
+);
--- src/packages/ecs-editor/components/SelectComponentDefinitionButton.tsx
+++ src/packages/ecs-editor/components/SelectComponentDefinitionButton.tsx
@@ -1,17 +1,9 @@
 import React from "react";
-import PopupState, { bindPopper, bindToggle } from "material-ui-popup-state";
-import {
-  ClickAwayListener,
-  Fade,
-  IconButton,
-  Paper,
-  Popper,
-  TextField,
-  Tooltip,
-} from "@material-ui/core";
+import { IconButton, TextField, Tooltip } from "@material-ui/core";
 import { Autocomplete } from "@material-ui/lab";
 import { ComponentDefinition } from "../../ecs-serializable/types/ComponentDefinition";
 import { AddIcon } from "./icons";
+import { CommonPopper } from "./CommonPopper";
 
 export type SelectComponentDefinitionButtonProps = {
   componentDefinitions: ComponentDefinition[];
@@ -22,45 +14,30 @@ export const SelectComponentDefinitionButton = ({
   componentDefinitions,
   onSelected,
 }: SelectComponentDefinitionButtonProps) => (
-  <PopupState variant="popper" popupId="demo-popup-popper">
-    {(popupState) => (
-      <div>
-        <Tooltip title="Add component">
-          <IconButton
-            edge="end"
-            aria-label="delete"
-            {...bindToggle(popupState)}
-          >
-            <AddIcon />
-          </IconButton>
-        </Tooltip>
-        <Popper {...bindPopper(popupState)} placement="bottom-end" transition>
-          {({ TransitionProps }) => (
-            <Fade {...TransitionProps} timeout={350}>
-              <Paper style={{ width: 300 }}>
-                <ClickAwayListener onClickAway={popupState.close}>
-                  <Autocomplete
-                    onChange={(e, definition) => {
-                      if (definition) {
-                        onSelected(definition);
-                      }
-                    }}
-                    renderInput={(params) => (
-                      <TextField
-                        {...params}
-                        placeholder="Select component"
-                        variant="outlined"
-                      />
-                    )}
-                    getOptionLabel={(definition) => definition.name}
-                    options={componentDefinitions}
-                  />
-                </ClickAwayListener>
-              </Paper>
-            </Fade>
-          )}
-        </Popper>
-      </div>
+  <CommonPopper
+    toggle={(props) => (
+      <Tooltip title="Add component">
+        <IconButton edge="end" aria-label="delete" {...props}>
+          <AddIcon />
+        </IconButton>
+      </Tooltip>
     )}
-  </PopupState>
+  >
+    <Autocomplete
+      onChange={(e, definition) => {
+        if (definition) {
+          onSelected(definition);
+        }
+      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          placeholder="Select component"
+          variant="outlined"
+        />
+      )}
+      getOptionLabel={(definition) => definition.name}
+      options={componentDefinitions}
+    />
+  </CommonPopper>
 );
