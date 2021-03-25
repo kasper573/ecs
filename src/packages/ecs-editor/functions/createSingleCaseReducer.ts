39caaffcb1e9db3391d1051e0dd87424aea81db0
Kasper Sandin <kasper.sandin@gmail.com>
2021-03-25 09:42:28 +0100

    Remove redundant function

--- src/packages/ecs-editor/functions/createSingleCaseReducer.ts
+++ src/packages/ecs-editor/functions/createSingleCaseReducer.ts
@@ -1,9 +1,9 @@
-import { Action, createReducer, Reducer } from "@reduxjs/toolkit";
+import { Action, CaseReducer, createReducer, Reducer } from "@reduxjs/toolkit";
 
 export const createSingleCaseReducer = <S, A extends Action>(
   initialState: S,
   reducer: (state: S, action: A) => S | void
 ): Reducer<S, A> =>
   createReducer<S>(initialState, (builder) =>
-    builder.addDefaultCase((state, action) => reducer(state as S, action as A))
+    builder.addDefaultCase((reducer as unknown) as CaseReducer<S>)
   );
