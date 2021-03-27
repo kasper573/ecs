999266c1f20a67bc20cd9ed7cfaa48a2ba2b9f60
Kasper Sandin <kasper.sandin@gmail.com>
2021-03-27 21:56:38 +0100

    Make createShallowSelector draft safe

--- src/packages/ecs-editor/functions/createShallowSelector.ts
+++ src/packages/ecs-editor/functions/createShallowSelector.ts
@@ -1,14 +1,26 @@
 import { shallowEqual } from "react-redux";
 import { createSelectorCreator, defaultMemoize } from "reselect";
+import { current, isDraft } from "immer";
 
-export const createShallowSelector: SpecificCreateShallowSelector = createSelectorCreator(
-  defaultMemoize,
-  shallowEqual
-);
+const shallowCreator = createSelectorCreator(defaultMemoize, shallowEqual);
+
+const makeDraftSafe = (selector: Function) => (
+  value: unknown,
+  ...rest: unknown[]
+) => selector(isDraft(value) ? current(value) : value, ...rest);
+
+const draftSafeShallowCreator = (...args: any[]) =>
+  makeDraftSafe((shallowCreator as any)(...args));
+
+/**
+ * Creates a draft safe selector with default memoization
+ * but shallow equality check for parameters to memoize on.
+ */
+export const createShallowSelector: CreateShallowSelectorWithParams = draftSafeShallowCreator;
 
 // createSelectorCreator has trouble inferring types properly,
 // so we use this specific type definition that fits our use case.
-type SpecificCreateShallowSelector = <
+type CreateShallowSelectorWithParams = <
   SelectParams extends (...args: any) => any,
   Select extends (params: ReturnType<SelectParams>) => any
 >(
