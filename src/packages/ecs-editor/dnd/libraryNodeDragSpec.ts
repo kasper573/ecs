dd2de0e486acaa33a604555517aafe7fc1c352d3
Kasper Sandin <kasper.sandin@gmail.com>
2021-03-31 04:09:03 +0200

    Remove unused collect

--- src/packages/ecs-editor/dnd/libraryNodeDragSpec.ts
+++ src/packages/ecs-editor/dnd/libraryNodeDragSpec.ts
@@ -1,4 +1,3 @@
-import { DragSourceMonitor } from "react-dnd";
 import { DiscriminatedLibraryNode } from "../types/DiscriminatedLibraryNode";
 import { DNDType } from "./DNDType";
 
@@ -6,10 +5,6 @@ export const libraryNodeDragSpec = (node: DiscriminatedLibraryNode) => ({
   options: { dropEffect: "move" },
   type: getDragTypeForNode(node),
   item: node as unknown,
-  collect: (monitor: DragSourceMonitor) => ({
-    isDragging: monitor.isDragging(),
-    node,
-  }),
 });
 
 function getDragTypeForNode(node: DiscriminatedLibraryNode) {
