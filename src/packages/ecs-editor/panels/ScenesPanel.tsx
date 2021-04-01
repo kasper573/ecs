e8c3c8aed55033031a10f997f5566a734bf43e50
Kasper Sandin <kasper.sandin@gmail.com>
2021-04-01 22:00:22 +0200

    Replace useCrudDialogs with useDialog in ScenesPanel.tsx

--- src/packages/ecs-editor/panels/ScenesPanel.tsx
+++ src/packages/ecs-editor/panels/ScenesPanel.tsx
@@ -1,67 +1,82 @@
 import { IconButton, Tooltip } from "@material-ui/core";
-import React, { useCallback } from "react";
 import { PanelName } from "../types/PanelName";
 import { PanelHeader } from "../components/PanelHeader";
 import { useDispatch, useSelector } from "../store";
 import { AddIcon, SceneIcon } from "../icons";
 import { CrudList } from "../components/CrudList";
 import { core } from "../core";
-import { omit } from "../../ecs-common/omit";
 import { Panel } from "../components/Panel";
-import { useCrudDialogs } from "../hooks/useCrudDialogs";
 import { SceneDefinition } from "../../ecs-serializable/types/SceneDefinition";
 import { uuid } from "../../ecs-common/uuid";
 import { selectListOfSceneDefinition } from "../selectors/selectListOfSceneDefinition";
 import { selectSelectedSystemDefinition } from "../selectors/selectSelectedSystemDefinition";
 import { selectSelectedSceneDefinition } from "../selectors/selectSelectedSceneDefinition";
+import { useDialog } from "../hooks/useDialog";
+import { NameDialog } from "../components/NameDialog";
+import { DeleteDialog } from "../components/DeleteDialog";
 
 export const ScenesPanel = () => {
   const selectedSystem = useSelector(selectSelectedSystemDefinition);
   const selectedScene = useSelector(selectSelectedSceneDefinition);
   const scenes = useSelector(selectListOfSceneDefinition);
   const dispatch = useDispatch();
-  const [sceneEvents, sceneDialogs] = useCrudDialogs<SceneDefinition>({
-    createDialogTitle: "Add scene",
-    getItemName: (item) => item.name,
-    onCreateItem: (name) =>
-      dispatch(
-        core.actions.createSceneDefinition({
-          id: uuid(),
-          name,
-          systemId: selectedSystem?.id!,
-        })
-      ),
-    onRenameItem: (scene, name) =>
-      dispatch(
-        core.actions.renameSceneDefinition({
-          sceneId: scene.id,
-          name,
-        })
-      ),
-    onDeleteItem: (scene) =>
-      dispatch(core.actions.deleteSceneDefinition(scene.id)),
-  });
-  const handleDuplicate = useCallback(
-    (scene: SceneDefinition) =>
-      dispatch(core.actions.duplicateSceneDefinition(scene.id)),
-    [dispatch]
-  );
-  const handleSceneSelected = useCallback(
-    (scene: SceneDefinition) =>
-      dispatch(core.actions.setSelectedSceneDefinition(scene.id)),
-    [dispatch]
-  );
+
+  const showCreateDialog = useDialog((props) => (
+    <NameDialog {...props} title="New scene" onSave={handleCreate} />
+  ));
+
+  const showRenameDialog = useDialog((props, scene: SceneDefinition) => (
+    <NameDialog
+      {...props}
+      title={`Rename ${scene.name}`}
+      defaultValue={scene.name}
+      onSave={(name) => handleRename(scene, name)}
+    />
+  ));
+
+  const showDeleteDialog = useDialog((props, scene: SceneDefinition) => (
+    <DeleteDialog
+      {...props}
+      name={scene.name}
+      onDelete={() => handleDelete(scene)}
+    />
+  ));
+
+  function handleCreate(name: string) {
+    dispatch(
+      core.actions.createSceneDefinition({
+        id: uuid(),
+        name,
+        systemId: selectedSystem?.id!,
+      })
+    );
+  }
+
+  function handleRename(scene: SceneDefinition, name: string) {
+    dispatch(core.actions.renameSceneDefinition({ sceneId: scene.id, name }));
+  }
+
+  function handleDuplicate(scene: SceneDefinition) {
+    dispatch(core.actions.duplicateSceneDefinition(scene.id));
+  }
+
+  function handleSelected(scene: SceneDefinition) {
+    dispatch(core.actions.setSelectedSceneDefinition(scene.id));
+  }
+
+  function handleDelete(scene: SceneDefinition) {
+    dispatch(core.actions.deleteSceneDefinition(scene.id));
+  }
 
   return (
     <Panel name={PanelName.Scenes}>
-      {sceneDialogs}
       <PanelHeader title={PanelName.Scenes}>
         {selectedSystem && (
-          <Tooltip title="Add scene">
+          <Tooltip title="New scene">
             <IconButton
               edge="end"
-              aria-label="add scene"
-              onClick={sceneEvents.onCreateItem}
+              aria-label="New scene"
+              onClick={showCreateDialog}
             >
               <AddIcon />
             </IconButton>
@@ -74,9 +89,10 @@ export const ScenesPanel = () => {
         items={scenes}
         getItemProps={getItemProps}
         getItemKey={getItemKey}
-        onSelectItem={handleSceneSelected}
+        onSelectItem={handleSelected}
         onDuplicateItem={handleDuplicate}
-        {...omit(sceneEvents, "onCreateItem")}
+        onDeleteItem={showDeleteDialog}
+        onUpdateItem={showRenameDialog}
       />
     </Panel>
   );
