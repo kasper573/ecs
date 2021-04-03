3d0217381c9b66b168562427d8b1d79bc80841cb
Kasper Sandin <kasper.sandin@gmail.com>
2021-04-03 16:14:02 +0200

    deserializeJS should return a function type

--- src/packages/ecs-serializable/jsSerializer.ts
+++ src/packages/ecs-serializable/jsSerializer.ts
@@ -1,5 +1,7 @@
 export { default as serializeJS } from "serialize-javascript";
 
-export const deserializeJS = (serializedJavascript: string) =>
+export const deserializeJS = <F extends (...args: any) => any>(
+  serializedJavascript: string
+): F =>
   // eslint-disable-next-line no-eval
   eval("(" + serializedJavascript + ")");
