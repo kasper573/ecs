46e8ff09cc76a4204a42c373c9313e1bfdbd3607
Kasper Sandin <kasper.sandin@gmail.com>
2021-04-03 18:50:24 +0200

    Avoid distributivity in Resolvable.ts

--- src/packages/property-bag/types/Resolvable.ts
+++ src/packages/property-bag/types/Resolvable.ts
@@ -1,5 +1,12 @@
-export type Resolvable<T> = T extends Function
+// NOTE we avoid distributivity by wrapping T in []
+// https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
+export type Resolvable<T> = [T] extends [(...args: any) => any]
   ? T
-  : T extends boolean
-  ? boolean | (() => boolean)
-  : T | (() => T);
+  : IfContains<
+      T,
+      undefined,
+      T | (() => void) | (() => T), // Also accept void functions, it's practically the same as undefined
+      T | (() => T)
+    >;
+
+type IfContains<T, S, A, B> = Extract<T, S> extends S ? A : B;
