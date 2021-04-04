9e07376a4f020bb588a74da8ba6f4d06397347c3
Kasper Sandin <kasper.sandin@gmail.com>
2021-04-04 20:05:06 +0200

    Add view inventory action to Inventory component

--- src/packages/ecs-collectable/Inventory.ts
+++ src/packages/ecs-collectable/Inventory.ts
@@ -1,13 +1,32 @@
-import { Component } from "../ecs/Component";
+import { componentProperties } from "../ecs/Component";
 import { Entity } from "../ecs/Entity";
 import { Container } from "../ecs/Container";
+import {
+  Interactive,
+  interactiveProperties,
+} from "../ecs-interactive/Interactive";
 
-export class Inventory extends Component {
+export class Inventory extends Interactive.extend({
+  isActive: { ...componentProperties.isActive, hidden: true },
+  action: { ...interactiveProperties.action, hidden: true },
+  effect: { ...interactiveProperties.effect, hidden: true },
+}) {
   readonly items = new Container<Entity>();
 
   static create(...items: Entity[]) {
     const inv = new Inventory();
     inv.items.push(...items);
     return inv;
+  }
+
+  constructor() {
+    super({
+      isActive: true,
+      action: "View inventory",
+      effect: () =>
+        this.items.length > 0
+          ? `Inventory: ${this.items.map((e) => e.name).join(", ")}`
+          : "Your inventory is empty!",
+    });
   }
 }
