0e103147f6e2010af140b05cf3a41db67326dcba
Kasper Sandin <kasper.sandin@gmail.com>
2021-04-06 01:27:38 +0200

    Remove mutations interface from descendants result

--- src/packages/ecs/descendants.ts
+++ src/packages/ecs/descendants.ts
@@ -1,11 +1,12 @@
 import { Entity } from "./Entity";
 import { EntityCollection } from "./EntityCollection";
+import { WithoutMutations } from "./WithoutMutations";
 
 export function descendants<Id extends string>(
   start: Entity<Id>,
   filter: (e: Entity<Id>) => boolean = any,
   includeSelf = false
-) {
+): WithoutMutations<EntityCollection<Id>> {
   const selected = new EntityCollection<Id>();
   const queue = includeSelf ? [start] : [...start.children];
   for (let i = 0; i < queue.length; i++) {
