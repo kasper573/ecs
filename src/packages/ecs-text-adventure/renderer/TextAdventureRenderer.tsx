7e98f9108bf7dfa99c201652f0cdecbc01a2c387
Kasper Sandin <kasper.sandin@gmail.com>
2021-04-06 23:22:07 +0200

    Respect isActive property in TextAdventureRenderer

--- src/packages/ecs-text-adventure/renderer/TextAdventureRenderer.tsx
+++ src/packages/ecs-text-adventure/renderer/TextAdventureRenderer.tsx
@@ -1,4 +1,4 @@
-import { render } from "react-dom";
+import { render, unmountComponentAtNode } from "react-dom";
 import { Component } from "../../ecs/Component";
 import { TextSystem } from "./TextSystem";
 
@@ -6,11 +6,19 @@ export class TextAdventureRenderer extends Component {
   get renderTarget() {
     return this.entity?.system?.getContext<Element>("renderTarget");
   }
+  clearRenderTarget() {
+    if (this.renderTarget) {
+      unmountComponentAtNode(this.renderTarget);
+    }
+  }
   constructor() {
     super({
+      mount: () => () => this.clearRenderTarget(),
       update: () => {
         const target = this.renderTarget;
-        if (target instanceof Element) {
+        if (!this.isActive) {
+          this.clearRenderTarget();
+        } else if (target instanceof Element) {
           render(<TextSystem system={this.entity?.system!} />, target);
         }
       },
