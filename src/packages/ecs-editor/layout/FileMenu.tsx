b1439b4bd3d3667e20d3a5939cc98fbae85baab8
Kasper Sandin <kasper.sandin@gmail.com>
2021-04-07 21:35:31 +0200

    Add intro for file menu

--- src/packages/ecs-editor/layout/FileMenu.tsx
+++ src/packages/ecs-editor/layout/FileMenu.tsx
@@ -6,28 +6,42 @@ import {
 } from "@material-ui/core";
 import { MenuIcon } from "../icons";
 import { useSelector } from "../store";
-import { selectListOfSystemDefinition } from "../selectors/selectListOfSystemDefinition";
 import { MenuFor } from "../components/MenuFor";
 import { useSystemCrud } from "../hooks/useSystemCrud";
+import { Intro } from "../../intro/Intro";
+import { selectHasSystems } from "../selectors/selectHasSystems";
 
 export const FileMenu = (buttonProps: IconButtonProps) => {
-  const systems = useSelector(selectListOfSystemDefinition);
+  const hasSystems = useSelector(selectHasSystems);
   const { showCreateDialog, showSelectDialog } = useSystemCrud();
   return (
     <MenuFor
       items={[
         <MenuItem onClick={showCreateDialog}>New system</MenuItem>,
-        systems.length > 0 && (
+        hasSystems && (
           <MenuItem onClick={showSelectDialog}>Select system</MenuItem>
         ),
       ]}
     >
       {(props) => (
-        <Tooltip title="File">
-          <IconButton {...props} {...buttonProps}>
-            <MenuIcon />
-          </IconButton>
-        </Tooltip>
+        <Intro
+          introId="WhereAreTheSystems"
+          message="You can create additional or switch systems in the file menu."
+          when={hasSystems}
+        >
+          {({ isIntroVisible }) => {
+            const button = (
+              <IconButton {...props} {...buttonProps}>
+                <MenuIcon />
+              </IconButton>
+            );
+            if (isIntroVisible) {
+              // Avoid material-ui warning about nested tooltips
+              return button;
+            }
+            return <Tooltip title="File">{button}</Tooltip>;
+          }}
+        </Intro>
       )}
     </MenuFor>
   );
