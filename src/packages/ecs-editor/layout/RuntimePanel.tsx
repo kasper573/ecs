25ffd38abc1082c31dc891fdd55f8f771bac19d1
Kasper Sandin <kasper.sandin@gmail.com>
2021-04-07 23:13:49 +0200

    Don't nest tooltips in RuntimePanel

--- src/packages/ecs-editor/layout/RuntimePanel.tsx
+++ src/packages/ecs-editor/layout/RuntimePanel.tsx
@@ -6,23 +6,29 @@ import { Panel } from "../components/Panel";
 import { PanelName } from "../types/PanelName";
 import { PanelHeader } from "../components/PanelHeader";
 import { RenderTarget } from "../components/RenderTarget";
-import { Intro } from "../intro/Intro";
+import { IntroWithDefaultTooltip } from "../intro/IntroWithDefaultTooltip";
 
 export const RuntimePanel = () => {
   const [system, resetSystem] = useSystemSync();
   return (
     <Panel name={PanelName.Runtime}>
       <PanelHeader title={PanelName.Runtime}>
-        <Intro
+        <IntroWithDefaultTooltip
+          defaultTooltip={{ title: "Reset runtime" }}
           introId="WhatIsRuntimeReset"
-          message="Sometimes it's useful to force reset the runtime. Press reset runtime to return the current system to its original state."
+          message={
+            "Sometimes it's useful to force reset the runtime. " +
+            "Press reset runtime to return the current system to its original state."
+          }
         >
-          <Tooltip title="Reset runtime" onClick={resetSystem}>
-            <IconButton edge="end" aria-label="Reset runtime">
-              <ResetIcon />
-            </IconButton>
-          </Tooltip>
-        </Intro>
+          <IconButton
+            edge="end"
+            aria-label="Reset runtime"
+            onClick={resetSystem}
+          >
+            <ResetIcon />
+          </IconButton>
+        </IntroWithDefaultTooltip>
       </PanelHeader>
       <RenderTarget system={system} />
     </Panel>
