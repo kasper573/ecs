7d5e3074cbaf3fc05f8d09237d09efea3081fa35
Kasper Sandin <kasper.sandin@gmail.com>
2021-04-13 17:40:14 +0200

    Delete related entity initializers when deleting systems

--- packages/ecs-editor/src/reducers/deleteSystemDefinition.ts
+++ packages/ecs-editor/src/reducers/deleteSystemDefinition.ts
@@ -5,13 +5,25 @@ import { core } from "../core";
 import { deleteEntityDefinition } from "./deleteEntityDefinition";
 import { deleteLibraryFolder } from "./deleteLibraryFolder";
 import { deleteComponentDefinition } from "./deleteComponentDefinition";
+import { deleteEntityInitializer } from "./deleteEntityInitializer";
 
 export const deleteSystemDefinition = createEditorStateReducer<SystemDefinitionId>(
   (state, { payload: id }) => {
     if (!removeNominal(state.ecs.systems, id)) {
       throw new Error("Could not delete system");
     }
-    // Delete related objects
+
+    // Delete only root entities because deleteEntityInitializer deletes descendants automatically
+    for (const init of Object.values(state.ecs.entityInitializers).filter(
+      (def) => !def.parentId && def.systemId === id
+    )) {
+      deleteEntityInitializer(
+        state,
+        core.actions.deleteEntityInitializer(init.id)
+      );
+    }
+
+    // Delete all entity/component definitions
     for (const def of Object.values(state.ecs.entityDefinitions).filter(
       (def) => def.systemId === id
     )) {
@@ -29,7 +41,7 @@ export const deleteSystemDefinition = createEditorStateReducer<SystemDefinitionI
       );
     }
 
-    // Delete only root folders because deleteLibraryFolder deletes ancestors already
+    // Delete only root folders because deleteLibraryFolder deletes descendants automatically
     const rootFolders = Object.values(state.ecs.libraryFolders).filter(
       (folder) => !folder.parentNodeId && folder.systemId === id
     );
