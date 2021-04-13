bd811ca3d83981c34e6fd2337b61594a7c41f121
Kasper Sandin <kasper.sandin@gmail.com>
2021-04-13 22:33:12 +0200

    Refresh game when system updates

--- packages/example-game/src/Game.tsx
+++ packages/example-game/src/Game.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import styled from "styled-components";
 import { System } from "../../ecs/src/System";
 import { TextSystem } from "../../ecs-text-adventure/src/renderer/TextSystem";
@@ -9,20 +9,29 @@ export type GameProps = {
   votesPerAction: number[];
 };
 
-export const Game = ({ system, timeLeft, votesPerAction }: GameProps) => (
-  <MaximizedTextSystem
-    system={system}
-    describers={{
-      describeAction: (action, index) =>
-        `${index + 1}. ${action.name} (${votesPerAction[index] || 0} votes)`,
-    }}
-  >
-    <div>Time left: {Math.round(timeLeft / 1000)}s</div>
-    <VoteInstruction>
-      Vote by typing one of the action numbers into twitch chat.
-    </VoteInstruction>
-  </MaximizedTextSystem>
-);
+export const Game = ({ system, timeLeft, votesPerAction }: GameProps) => {
+  const [, refresh] = useReducer((n) => n + 1, 0);
+  useEffect(() => {
+    system.events.on("update", refresh);
+    return () => {
+      system.events.off("update", refresh);
+    };
+  }, [system]);
+  return (
+    <MaximizedTextSystem
+      system={system}
+      describers={{
+        describeAction: (action, index) =>
+          `${index + 1}. ${action.name} (${votesPerAction[index] || 0} votes)`,
+      }}
+    >
+      <div>Time left: {Math.round(timeLeft / 1000)}s</div>
+      <VoteInstruction>
+        Vote by typing one of the action numbers into twitch chat.
+      </VoteInstruction>
+    </MaximizedTextSystem>
+  );
+};
 
 const VoteInstruction = styled.div`
   position: absolute;
