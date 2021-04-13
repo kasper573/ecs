2379f8108be5d6769f77188caa205f0db11ea7eb
Kasper Sandin <kasper.sandin@gmail.com>
2021-04-13 22:05:08 +0200

    Move native components record to separate package

--- packages/ecs-editor/src/index.tsx
+++ packages/ecs-editor/src/index.tsx
@@ -3,16 +3,9 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { Describable } from "../../ecs-text-adventure/src/describable/Describable";
-import { Collectable } from "../../ecs-text-adventure/src/collectable/Collectable";
-import { Interactive } from "../../ecs-text-adventure/src/interactive/Interactive";
-import { Inventory } from "../../ecs-text-adventure/src/collectable/Inventory";
-import { InteractionMemory } from "../../ecs-text-adventure/src/interactive/InteractionMemory";
-import { SceneManager } from "../../ecs-scene-manager/src/SceneManager";
-import { SceneSwitch } from "../../ecs-scene-manager/src/SceneSwitch";
-import { TextAdventureRenderer } from "../../ecs-text-adventure/src/renderer/TextAdventureRenderer";
 import { createECSDefinition } from "../../ecs-serializable/src/functions/createECSDefinition";
 import { reaction } from "../../ecs-common/src/reaction";
+import nativeComponents from "../../ecs-native-components";
 import { createEditorState } from "./functions/createEditorState";
 import { NativeComponentsContext } from "./NativeComponentsContext";
 import { createStore } from "./store";
@@ -21,17 +14,6 @@ import {
   loadECSDefinitionFromLocalStorage,
   saveECSDefinitionToLocalStorage,
 } from "./storage/lsECSDefinition";
-
-export const nativeComponents = {
-  Describable,
-  Collectable,
-  Interactive,
-  Inventory,
-  InteractionMemory,
-  SceneManager,
-  SceneSwitch,
-  TextAdventureRenderer,
-};
 
 const store = createStore({
   ...createEditorState(),
@@ -60,9 +42,3 @@ function render() {
 }
 
 render();
-
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://snowpack.dev/concepts/hot-module-replacement
-// if (import.meta.hot) {
-//   import.meta.hot.accept();
-// }
