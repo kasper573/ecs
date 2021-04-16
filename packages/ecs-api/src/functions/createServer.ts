bb239180cf0cde467277ada8b333c243e82152ca
Kasper Sandin <kasper.sandin@gmail.com>
2021-04-16 10:41:02 +0200

    Add SSL support to api

--- packages/ecs-api/src/functions/createServer.ts
+++ packages/ecs-api/src/functions/createServer.ts
@@ -1,10 +1,11 @@
+import https from "https";
 import { default as express, Express, Handler } from "express";
 import cors from "cors";
 import morgan from "morgan";
 import { ServerSettings } from "./loadServerSettings";
 
 export function createServer(
-  { origin, morganFormat }: ServerSettings,
+  { origin, morganFormat, ssl }: ServerSettings,
   routes: ServerRoute[]
 ) {
   // Initialize app and add middlewares
@@ -16,6 +17,10 @@ export function createServer(
 
   // Add api routes
   routes.forEach((route) => app[route.method](route.path, route.handlers));
+  if (ssl) {
+    console.log("Using SSL");
+    return https.createServer(ssl, app);
+  }
   return app;
 }
 
