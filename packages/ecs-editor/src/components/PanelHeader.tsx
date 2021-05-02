dfd834d436158bd0cc68f110e43e62a784a50f49
Kasper Sandin <kasper.sandin@gmail.com>
2021-05-03 00:59:28 +0200

    Disable PanelHeader
    Will reinstate PanelHeader as soon as window feature allows it

--- packages/ecs-editor/src/components/PanelHeader.tsx
+++ packages/ecs-editor/src/components/PanelHeader.tsx
@@ -1,12 +1,15 @@
-import { ListItemSecondaryAction } from "@material-ui/core";
-import React, { PropsWithChildren, ReactNode } from "react";
-import { OpaqueListSubheader } from "./OpaqueListSubheader";
+//import { ListItemSecondaryAction } from "@material-ui/core";
+import { PropsWithChildren, ReactNode } from "react";
+//import { OpaqueListSubheader } from "./OpaqueListSubheader";
 
 export type PanelHeaderProps = PropsWithChildren<{ title: ReactNode }>;
 
-export const PanelHeader = ({ title, children }: PanelHeaderProps) => (
-  <OpaqueListSubheader>
-    {title}
-    <ListItemSecondaryAction>{children}</ListItemSecondaryAction>
-  </OpaqueListSubheader>
-);
+export const PanelHeader = ({ title, children }: PanelHeaderProps) => {
+  return null;
+  // return (
+  //   <OpaqueListSubheader>
+  //     {title}
+  //     <ListItemSecondaryAction>{children}</ListItemSecondaryAction>
+  //   </OpaqueListSubheader>
+  // )
+};
