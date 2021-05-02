98650d6328cd3c93f0442648030fd136dec822d3
Kasper Sandin <kasper.sandin@gmail.com>
2021-05-02 23:00:16 +0200

    Explain open window mechanism

--- packages/ecs-editor/src/features/window/reducers/openWindow.ts
+++ packages/ecs-editor/src/features/window/reducers/openWindow.ts
@@ -12,10 +12,13 @@ export const openWindow = createEditorStateReducer<WindowId>(
       return;
     }
     if (!state.windows) {
+      // Opening the first and only window
       state.windows = windowId;
     } else if (!isParent(state.windows)) {
+      // Opening a second window. Put side by side.
       state.windows = join(windowId, state.windows, "row");
     } else {
+      // At least two windows open. Put new window beneath first slot.
       state.windows = {
         ...state.windows,
         first: join(state.windows.first, windowId, "column"),
